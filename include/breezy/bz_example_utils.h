8c5ef4aa3a64bcc2900c0770bb21125897bb8c3b
Ben <ben@zenittini.dev>
2026-04-22 13:52:50 -0400

    DL1: Set up basic build with utilities.

---
+++ include/breezy/bz_example_utils.h
@@ -0,0 +1,6 @@
+#ifndef BREEZE_BZ_EXAMPLE_UTILS_H
+#define BREEZE_BZ_EXAMPLE_UTILS_H
+
+int add(int a, int b);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "breezy/bz_example_utils.h"
+
+int main(void) {
+  const int result = add(1, 2);
+  printf("Hello %d\n", result);
+}
